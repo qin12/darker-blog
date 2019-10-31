35c58fe32542b93c9bf23066f6c10da5f8d88808
qin12 <1520290900@qq.com>
2019-10-31 13:43:47 +0800

    项目初始化

---
+++ src/main/java/com/darker/blog/DarkerBlogApplication.java
@@ -0,0 +1,13 @@
+package com.darker.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkerBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkerBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darker/blog/DarkerBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darker.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkerBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
